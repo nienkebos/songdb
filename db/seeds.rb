a5344e6586997347c59554b0c00ab1c36178577c
nienkebos <nienkebos@live.com>
2016-08-14 21:21:19 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+
+artist1 = Artist.create(name: "Leonard Cohen" )
+artist2 = Artist.create(name: "Moby" )
+artist3 = Artist.create(name: "The Doors" )
+artist4 = Artist.create(name: "Michael Jackson" )
+
+Song.create({title: "Famous blue raincoat", artist: artist1 })
+Song.create({title: "Why does my heart feel so bad?", artist: artist2 })
+Song.create({title: "Light my fire", artist: artist3 })
+Song.create({title: "Thriller", artist: artist4 })
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
