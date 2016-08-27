50e8c47bedd118a6014cadace0fb231eca65b22e
nienkebos <nienkebos@live.com>
2016-08-27 12:16:14 +0200

    List on view, routes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,8 @@ class Artist < ApplicationRecord
   has_many :songs
 
   mount_uploader :image, ImageUploader
+
+  def self.order_by_name
+    order(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'artists/index'
 
-  get 'artists/show'
+  root "artists#index"
+
+  get "artists" => "artists#index"
+  get "artists/:id" => "artists#show", as: :artist
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
