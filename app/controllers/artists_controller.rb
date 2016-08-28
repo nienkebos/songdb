7eaebc13b4ce399a17818c65aef74bd87208db4c
nienkebos <nienkebos@live.com>
2016-08-28 13:54:00 +0200

    Sort method

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,23 @@
 class ArtistsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+
+
   def index
-    @artists = Artist.all
+    @artists = Artist.order(params[:sort] + ' ' + params[:direction])
   end
 
   def show
     @artist = Artist.find(params[:id])
   end
+
+  private
+  def sort_column
+    params[:sort] || "name"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
+
 end
