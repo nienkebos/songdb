1aa9de99d4f53c255211569ba791ec5414e78dc0
nienkebos <nienkebos@live.com>
2016-08-28 22:15:46 +0200

    Added thumbnails to artists index

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ Song.delete_all
 Artist.delete_all
 
 
-artist1 = Artist.create(name: "Leonard Cohen", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Leonard_Cohen_2187-edited.jpg/800px-Leonard_Cohen_2187-edited.jpg" )
-artist2 = Artist.create(name: "Moby", image: "http://www.gosnowmass.com/wp-content/uploads/2015/02/moby_showcase_1.jpg" )
-artist3 = Artist.create(name: "The Doors", image: "http://theredshtick.com/wp-content/uploads/2014/07/the-doors.jpg" )
-artist4 = Artist.create(name: "Michael Jackson", image: "http://www.mtv.com/shared/promoimages/bands/j/jackson_michael/a-z/Michael%20Jackson%20Getty%2020.jpg" )
+artist1 = Artist.create( name: "Leonard Cohen", image: "http://res.cloudinary.com/du6v0bsg5/image/upload/v1472387400/Leonard_Cohen_vkibii.jpg" )
+artist2 = Artist.create( name: "Moby", image: "http://res.cloudinary.com/du6v0bsg5/image/upload/v1472387471/moby_j6k7mg.jpg" )
+artist3 = Artist.create( name: "The Doors", image: "http://res.cloudinary.com/du6v0bsg5/image/upload/v1472387531/the-doors_qhsa3b.jpg" )
+artist4 = Artist.create( name: "Michael Jackson", image: "http://res.cloudinary.com/du6v0bsg5/image/upload/v1472387576/Michael-Jackson_pamzp3.jpg" )
 
 Song.create([
   { title: "Famous blue raincoat", artist: artist1 },
